153639a58652134860f64f4151e1cc19e9fb5528
Mark Chen <chenmark33@gmail.com>
2020-09-02 03:25:54 -0400

    Added 1.1 Is Unique

--- src/Chapter01.java
+++ src/Chapter01.java
@@ -1,7 +1,22 @@
+import java.util.*;
+
 public class Chapter01 {
 
     public static void main(String[] args) {
         System.out.println("== Chapter 01 ==");
     }
-    
+
+    public boolean Q1IsUnique(String s) {
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (set.contains(c)) return false;
+            set.add(c);
+        }
+        return true;
+    }
+
+    public boolean Q2CheckPermutation(String a, String b) {
+        return true;
+    }
 }
